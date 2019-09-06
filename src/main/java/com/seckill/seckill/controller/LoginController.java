6113e8e7d1f4021d1c7668272ebd7980764764ed
Yuanzhe Yang <yang.4766@osu.edu>
2019-09-06 12:18:33 -0400

    login validation

--- src/main/java/com/seckill/seckill/controller/LoginController.java
+++ src/main/java/com/seckill/seckill/controller/LoginController.java
@@ -1,13 +1,35 @@
 package com.seckill.seckill.controller;
 
+import com.seckill.seckill.result.Result;
+import com.seckill.seckill.service.UserService;
+import com.seckill.seckill.vo.LoginVo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
+
+    private static Logger log = LoggerFactory.getLogger(LoginController.class);
+
+    @Autowired
+    UserService userService;
+
     @RequestMapping("/to_login")
     public String toLogin(){
         return "login";
+    }
+    @RequestMapping("/do_login")
+    @ResponseBody
+    public Result<Boolean> doLogin(LoginVo loginVo) {
+        log.info(loginVo.toString());
+        //参数 校验
+        String  passInput =  loginVo.getPassword();
+        String mobile = loginVo.getMobile();
+        return  null;
     }
 }
